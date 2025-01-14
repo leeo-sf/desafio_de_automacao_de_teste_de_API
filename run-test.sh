9679edde74f41ceca6652e947dd17779053d88c4
Leonardo Ferreira <leonardoferreira032@gmail.com>
2025-01-13 22:16:25 -0300

    remove arquivo sh e atualiza pipe

--- run-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-COLLECTION_PATH="./Desafio Banco Carrefour.postman_collection.json"
-ENVIRONMENT_PATH="./Desafio_CSF.postman_environment.json"
-
-npx newman run $COLLECTION_PATH -e $ENVIRONMENT_PATH
